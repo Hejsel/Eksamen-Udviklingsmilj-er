5d8b518a6dc5981775b3859dc3675e3255ab2bc7
Hejsel <benjaminhejsel@gmail.com>
2025-01-03 10:05:53 +0100

    Det resterende kode til canvas opgaven

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    // './src/pages/**/*.{html,js}', // Indsæt dine HTML- og JS-filer her
-    // './src/index.html',
-    // './src/pages/opg-1_git.html',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        dark: "#42000C", // Mørk rød
-        primary: "#E8F1E0", // Lys grønlig hvid
-        accent: "#FF4014", // Kraftig rød-orange
-        highlight: "#FF6B00", // Lys orange
-      },
-      spacing: {
-        "20pct": "20%", // Eksempel spacing som clamp
-      },
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "4rem",
-          xl: "5rem",
-        },
-      },
-    },
-  },
-  plugins: [],
+	content: ['./public/**/*.{html,js}', './src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				dark: '#42000C', // Mørk rød
+				primary: '#E8F1E0', // Lys grønlig hvid
+				accent: '#FF4014', // Kraftig rød-orange
+				highlight: '#FF6B00', // Lys orange
+				bgdarkmode: '#1e1e1e', // Mørkegrå
+				bgui: '#333333',
+				offwhite: '#cccccc', // Offwhite
+			},
+			spacing: {
+				'20pct': '20%', // Eksempel spacing som clamp
+			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '4rem',
+					xl: '5rem',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
