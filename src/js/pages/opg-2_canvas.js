9dd91430c14f1ddba5c1b4fd666ba57641affb19
emilroed <emilroed1994@hotmail.com>
2025-01-10 09:46:18 +0100

    um done

--- src/js/pages/opg-2_canvas.js
+++ src/js/pages/opg-2_canvas.js
@@ -1,15 +1,15 @@
-const lærred = document.getElementById('staticDrawing');
-const kontekst = lærred.getContext('2d');
+const lærred = document.getElementById("staticDrawing");
+const kontekst = lærred.getContext("2d");
 const tilRadianer = (grader) => (Math.PI / 180) * grader;
 
-kontekst.fillStyle = 'red';
+kontekst.fillStyle = "red";
 kontekst.fillRect(25, 25, 100, 100); // top, left, bredde, højde
 kontekst.clearRect(45, 45, 60, 60);
 kontekst.lineWidth = 5;
-kontekst.strokeStyle = 'green';
+kontekst.strokeStyle = "green";
 kontekst.strokeRect(50, 50, 50, 50);
 kontekst.lineWidth = 2;
-kontekst.strokeStyle = 'blue';
+kontekst.strokeStyle = "blue";
 kontekst.strokeRect(55, 55, 40, 40);
 
 kontekst.beginPath();
@@ -17,7 +17,7 @@ kontekst.moveTo(50, 150);
 kontekst.lineTo(150, 200);
 kontekst.lineTo(100, 250);
 kontekst.closePath(); // Danner en linje mellem sidste angivet punkt til det første angivne punkt i pathen.
-kontekst.strokeStyle = 'red';
+kontekst.strokeStyle = "red";
 //kontekst.stroke();
 kontekst.fill();
 
@@ -33,34 +33,205 @@ kontekst.stroke();
 
 kontekst.beginPath();
 kontekst.arc(150, 150, 100, 0, tilRadianer(90), false); // Bue fra 0 til 90 grader (⟳)
-kontekst.strokeStyle = 'green';
+kontekst.strokeStyle = "green";
 kontekst.lineWidth = 50;
 kontekst.stroke();
 
 for (let i = 0; i < 4; i++) {
-	for (let j = 0; j < 3; j++) {
-		kontekst.beginPath();
-		const x = 400 + j * 50; // x-koordinat
-		const y = 400 + i * 50; // y-koordinat
-		const radius = 20; // Radius
-		const startAngle = tilRadianer(0); // Startvinkel i grader
-		const endAngle = tilRadianer(180) + tilRadianer(90) * j; // Slutvinkel i grader
-		const counterclockwise = i % 2 !== 0; // Mod uret på ulige rækker
+  for (let j = 0; j < 3; j++) {
+    kontekst.beginPath();
+    const x = 400 + j * 50; // x-koordinat
+    const y = 400 + i * 50; // y-koordinat
+    const radius = 20; // Radius
+    const startAngle = tilRadianer(0); // Startvinkel i grader
+    const endAngle = tilRadianer(180) + tilRadianer(90) * j; // Slutvinkel i grader
+    const counterclockwise = i % 2 !== 0; // Mod uret på ulige rækker
 
-		// Tegn buen
-		kontekst.arc(x, y, radius, startAngle, endAngle, counterclockwise);
+    // Tegn buen
+    kontekst.arc(x, y, radius, startAngle, endAngle, counterclockwise);
 
-		// Sæt specifikke stilarter
-		kontekst.lineWidth = 2;
-		if (i > 1) {
-			kontekst.fillStyle = `rgb(${i * 60}, ${j * 40}, 120)`;
-			kontekst.fill();
-		} else {
-			kontekst.strokeStyle = `rgb(${j * 60}, ${i * 40}, 200)`;
-			kontekst.stroke();
-		}
-	}
+    // Sæt specifikke stilarter
+    kontekst.lineWidth = 2;
+    if (i > 1) {
+      kontekst.fillStyle = `rgb(${i * 60}, ${j * 40}, 120)`;
+      kontekst.fill();
+    } else {
+      kontekst.strokeStyle = `rgb(${j * 60}, ${i * 40}, 200)`;
+      kontekst.stroke();
+    }
+  }
 }
+
+const fileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#billedbehandling");
+const canvasCtx = canvas.getContext("2d");
+const brightnessInput = document.querySelector("#brightness");
+const saturationInput = document.querySelector("#saturation");
+const blurInput = document.querySelector("#blur");
+const inversionInput = document.querySelector("#inversion");
+
+const settings = {};
+let image = null;
+
+function resetSettings() {
+  settings.brightness = "100";
+  settings.saturation = "100";
+  settings.blur = "0";
+  settings.inversion = "0";
+
+  brightnessInput.value = settings.brightness;
+  saturationInput.value = settings.saturation;
+  blurInput.value = settings.blur;
+  inversionInput.value = settings.inversion;
+}
+
+function updateSetting(key, value) {
+  if (!image) return;
+
+  settings[key] = value;
+  renderImage();
+}
+
+function generateFilter() {
+  const { brightness, saturation, blur, inversion } = settings;
+
+  return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%)`;
+}
+
+function renderImage() {
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  canvasCtx.filter = generateFilter();
+  canvasCtx.drawImage(image, 0, 0);
+}
+
+brightnessInput.addEventListener("change", () =>
+  updateSetting("brightness", brightnessInput.value)
+);
+saturationInput.addEventListener("change", () =>
+  updateSetting("saturation", saturationInput.value)
+);
+blurInput.addEventListener("change", () => updateSetting("blur", blurInput.value));
+inversionInput.addEventListener("change", () => updateSetting("inversion", inversionInput.value));
+
+fileInput.addEventListener("change", () => {
+  image = new Image();
+
+  image.addEventListener("load", () => {
+    resetSettings();
+    renderImage();
+  });
+
+  image.src = URL.createObjectURL(fileInput.files[0]);
+});
+
+resetSettings();
+
+// Ur animation
+function clock() {
+  const now = new Date();
+  const artboard = document.getElementById("animatetClock");
+  const art = artboard.getContext("2d");
+  art.save();
+  art.clearRect(0, 0, 150, 150);
+  art.translate(75, 75);
+  art.scale(0.4, 0.4);
+  //art.rotate(tilRadianer(360));
+  art.rotate(-Math.PI / 2);
+  art.strokeStyle = "black";
+  art.fillStyle = "white";
+  art.lineWidth = 8;
+  art.lineCap = "round";
+
+  // Hour marks
+  art.save();
+  for (let index = 0; index < 12; index++) {
+    art.beginPath();
+    art.rotate(Math.PI / 6);
+    art.moveTo(100, 0);
+    art.lineTo(120, 0);
+    art.stroke();
+  }
+  art.restore();
+
+  // Minute marks
+  art.save();
+  art.lineWidth = 5;
+  for (let index = 0; index < 60; index++) {
+    if (index % 5 !== 0) {
+      art.beginPath();
+      art.moveTo(117, 0);
+      art.lineTo(120, 0);
+      art.stroke();
+    }
+    art.rotate(Math.PI / 30);
+  }
+  art.restore();
+
+  //const sec = now.getSeconds();
+  const sec = now.getSeconds() + now.getMilliseconds() / 1000;
+  const min = now.getMinutes();
+  const hr = now.getHours() % 12;
+
+  art.fillStyle = "black";
+
+  // Write image description
+  artboard.innerText = ` klokken er nu: ${hr}:${min}`;
+
+  // Write Hours
+  art.save();
+  art.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
+  art.lineWidth = 14;
+  art.beginPath();
+  art.moveTo(-20, 0);
+  art.lineTo(80, 0);
+  art.stroke();
+  art.restore();
+
+  // Write Minutes
+  art.save();
+  art.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  art.lineWidth = 10;
+  art.beginPath();
+  art.moveTo(-28, 0);
+  art.lineTo(112, 0);
+  art.stroke();
+  art.restore();
+
+  // Write seconds
+  art.save();
+  art.rotate((sec * Math.PI) / 30);
+  art.strokeStyle = "#D40000";
+  art.fillStyle = "#D40000";
+  art.lineWidth = 6;
+  art.beginPath();
+  art.moveTo(-30, 0);
+  art.lineTo(83, 0);
+  art.stroke();
+  art.beginPath();
+  art.arc(0, 0, 10, 0, Math.PI * 2, true);
+  art.fill();
+  art.beginPath();
+  art.arc(95, 0, 10, 0, Math.PI * 2, true);
+  art.stroke();
+  art.fillStyle = "rgb(0 0 0 / 0%)";
+  art.arc(0, 0, 3, 0, Math.PI * 2, true);
+  art.fill();
+  art.restore();
+
+  art.beginPath();
+  art.lineWidth = 14;
+  art.strokeStyle = "#325FA2";
+  art.arc(0, 0, 142, 0, Math.PI * 2, true);
+  art.stroke();
+
+  art.restore();
+
+  window.requestAnimationFrame(clock);
+}
+window.requestAnimationFrame(clock);
+
 /*
 // SVG-path-data
 const svgPath =
@@ -117,169 +288,3 @@ billede.onload = () => {
 	kontekst.putImageData(billedData, 0, 0);
 };
 */
-
-const fileInput = document.querySelector('#imageFileInput');
-const canvas = document.querySelector('#billedbehandling');
-const canvasCtx = canvas.getContext('2d');
-const brightnessInput = document.querySelector('#brightness');
-const saturationInput = document.querySelector('#saturation');
-const blurInput = document.querySelector('#blur');
-const inversionInput = document.querySelector('#inversion');
-
-const settings = {};
-let image = null;
-
-function resetSettings() {
-	settings.brightness = '100';
-	settings.saturation = '100';
-	settings.blur = '0';
-	settings.inversion = '0';
-
-	brightnessInput.value = settings.brightness;
-	saturationInput.value = settings.saturation;
-	blurInput.value = settings.blur;
-	inversionInput.value = settings.inversion;
-}
-
-function updateSetting(key, value) {
-	if (!image) return;
-
-	settings[key] = value;
-	renderImage();
-}
-
-function generateFilter() {
-	const { brightness, saturation, blur, inversion } = settings;
-
-	return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%)`;
-}
-
-function renderImage() {
-	canvas.width = image.width;
-	canvas.height = image.height;
-
-	canvasCtx.filter = generateFilter();
-	canvasCtx.drawImage(image, 0, 0);
-}
-
-brightnessInput.addEventListener('change', () => updateSetting('brightness', brightnessInput.value));
-saturationInput.addEventListener('change', () => updateSetting('saturation', saturationInput.value));
-blurInput.addEventListener('change', () => updateSetting('blur', blurInput.value));
-inversionInput.addEventListener('change', () => updateSetting('inversion', inversionInput.value));
-
-fileInput.addEventListener('change', () => {
-	image = new Image();
-
-	image.addEventListener('load', () => {
-		resetSettings();
-		renderImage();
-	});
-
-	image.src = URL.createObjectURL(fileInput.files[0]);
-});
-
-resetSettings();
-
-// Ur animation
-function clock() {
-	const now = new Date();
-	const artboard = document.getElementById('animatetClock');
-	const art = artboard.getContext('2d');
-	art.save();
-	art.clearRect(0, 0, 150, 150);
-	art.translate(75, 75);
-	art.scale(0.4, 0.4);
-	//art.rotate(tilRadianer(360));
-	art.rotate(-Math.PI / 2);
-	art.strokeStyle = 'black';
-	art.fillStyle = 'white';
-	art.lineWidth = 8;
-	art.lineCap = 'round';
-
-	// Hour marks
-	art.save();
-	for (let index = 0; index < 12; index++) {
-		art.beginPath();
-		art.rotate(Math.PI / 6);
-		art.moveTo(100, 0);
-		art.lineTo(120, 0);
-		art.stroke();
-	}
-	art.restore();
-
-	// Minute marks
-	art.save();
-	art.lineWidth = 5;
-	for (let index = 0; index < 60; index++) {
-		if (index % 5 !== 0) {
-			art.beginPath();
-			art.moveTo(117, 0);
-			art.lineTo(120, 0);
-			art.stroke();
-		}
-		art.rotate(Math.PI / 30);
-	}
-	art.restore();
-
-	//const sec = now.getSeconds();
-	const sec = now.getSeconds() + now.getMilliseconds() / 1000;
-	const min = now.getMinutes();
-	const hr = now.getHours() % 12;
-
-	art.fillStyle = 'black';
-
-	// Write image description
-	artboard.innerText = ` klokken er nu: ${hr}:${min}`;
-
-	// Write Hours
-	art.save();
-	art.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
-	art.lineWidth = 14;
-	art.beginPath();
-	art.moveTo(-20, 0);
-	art.lineTo(80, 0);
-	art.stroke();
-	art.restore();
-
-	// Write Minutes
-	art.save();
-	art.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
-	art.lineWidth = 10;
-	art.beginPath();
-	art.moveTo(-28, 0);
-	art.lineTo(112, 0);
-	art.stroke();
-	art.restore();
-
-	// Write seconds
-	art.save();
-	art.rotate((sec * Math.PI) / 30);
-	art.strokeStyle = '#D40000';
-	art.fillStyle = '#D40000';
-	art.lineWidth = 6;
-	art.beginPath();
-	art.moveTo(-30, 0);
-	art.lineTo(83, 0);
-	art.stroke();
-	art.beginPath();
-	art.arc(0, 0, 10, 0, Math.PI * 2, true);
-	art.fill();
-	art.beginPath();
-	art.arc(95, 0, 10, 0, Math.PI * 2, true);
-	art.stroke();
-	art.fillStyle = 'rgb(0 0 0 / 0%)';
-	art.arc(0, 0, 3, 0, Math.PI * 2, true);
-	art.fill();
-	art.restore();
-
-	art.beginPath();
-	art.lineWidth = 14;
-	art.strokeStyle = '#325FA2';
-	art.arc(0, 0, 142, 0, Math.PI * 2, true);
-	art.stroke();
-
-	art.restore();
-
-	window.requestAnimationFrame(clock);
-}
-window.requestAnimationFrame(clock);
